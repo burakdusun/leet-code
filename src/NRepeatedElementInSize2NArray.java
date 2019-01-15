dd4313f5f38f1856dfb5c611ca1466c577ae52e9
burakdusun <b.dusun@gmail.com>
2019-01-15 23:03:09 +0300

    Add solution for for n-repeated-element-in-size-2n-array
    link: https://leetcode.com/problems/n-repeated-element-in-size-2n-array
    
    TODO: runs faster than only %17 of other java submitters. Need to improve runtime.

---
+++ src/NRepeatedElementInSize2NArray.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+// solution for https://leetcode.com/problems/n-repeated-element-in-size-2n-array
+
+public class NRepeatedElementInSize2NArray {
+
+
+    public static void main(String[] args) {
+        System.out.println(repeatedNTimes(new int[]{1,2,3,3}));
+    }
+
+
+    private static int repeatedNTimes(int[] array) {
+        Map<Integer, Integer> intCountMap = new HashMap<>();
+        for (int element : array) {
+            int count;
+            if (!intCountMap.containsKey(element)) {
+                count = 1;
+                intCountMap.put(element, count);
+            } else {
+                count = intCountMap.get(element) + 1;
+                intCountMap.put(element, count);
+            }
+            if (count == (array.length) / 2) {
+                return element;
+            }
+        }
+        return -1;
+    }
+
+}
